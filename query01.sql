676107221b2c59fd8122dd0b97b0ea7bf52eb794
SidneyJeffries <sidney.r.jeffries@gmail.com>
2026-04-16 14:11:18 -0400

    Replacing LIMIT 5  with FETCH FIRST WITH TIES to include tied stops

--- query01.sql
+++ query01.sql
@@ -27,4 +27,4 @@ FROM septa_bus_stop_surrounding_population AS pop
 INNER JOIN septa.bus_stops AS stops USING (stop_id)
 WHERE pop.estimated_pop_800m > 500
 ORDER BY pop.estimated_pop_800m DESC
-LIMIT 5;
+FETCH FIRST 5 ROWS WITH TIES;
